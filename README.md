9e932c40c99fb4f11cb1cb4e40c3a1316a4c69d6
Vitor Bezerra <vitorbezera@gmail.com>
2021-11-02 15:26:23 -0300

    add video streaming

---
+++ README.md
@@ -0,0 +1,15 @@
+# Video Playback Server
+
+## Requirements
+```
+pip install opencv-python vidgear Flask
+```
+
+## Video Storage
+All videos must be on the ```MP4``` format in the ```static``` folder.
+
+
+## Run
+```
+python app.py
+```
--- app.py
+++ app.py
@@ -6,17 +6,19 @@ import cv2
 from flask import Flask, Response, render_template
 from vidgear.gears import VideoGear
 
-VIDEO_FOLDER = "/home/vbezerra/Documents/PROTECTOR/video_samples/"
+VIDEO_FOLDER = "static/"
 
 filenames = [f for f in listdir(VIDEO_FOLDER) if isfile(join(VIDEO_FOLDER, f))]
-filenames_without_extension = [name.split(".avi")[0] for name in filenames]
+filenames_without_extension = [name.split(".mp4")[0] for name in filenames]
 print(filenames_without_extension)
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 def gen(videoname):
     stream = VideoGear(source=videoname).start()
@@ -24,19 +26,33 @@ def gen(videoname):
         frame = stream.read()
         if frame is None:
             break
-        ret, buffer = cv2.imencode('.jpg', frame)
+        ret, buffer = cv2.imencode(".jpg", frame)
         frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+        yield (
+            b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+        )  # concat frame one by one and show result
 
-@app.route('/video/<id>')
+
+@app.route("/stream/<id>")
 def video_feed(id):
     video_name = str(id)
     if video_name not in filenames_without_extension:
         return Response("Not Found")
-    video_name = VIDEO_FOLDER + video_name + ".avi"
-    return Response(gen(video_name),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    video_name = VIDEO_FOLDER + video_name + ".mp4"
+    return Response(
+        gen(video_name), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+
+@app.route("/video/<id>/<timestamp>")
+def video_feed2(id, timestamp):
+    video_name = str(id)
+    timestamp = str(timestamp)
+    if video_name not in filenames_without_extension:
+        return Response("Not Found")
+    video_name = video_name + ".mp4"
+    return render_template("video_player.html", video_name=video_name, timestamp=5)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, threaded=True)
